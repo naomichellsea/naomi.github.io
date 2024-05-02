4971f3593b1de719ade44bdc6d399252323c5faf
naomichellsea <144774165+naomichellsea@users.noreply.github.com>
2024-05-02 12:08:38 +0400

    Add files via upload

---
+++ naomyst.js
@@ -0,0 +1,8 @@
+// Scroll animation for my project section
+ScrollReveal().reveal('.grid-item', {
+    delay: 300,
+    distance: '20px',
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    interval: 100
+});
