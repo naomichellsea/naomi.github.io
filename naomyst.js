3d4812fafe344f3ab13691df9174590a38589a69
naomichellsea <144774165+naomichellsea@users.noreply.github.com>
2024-05-02 12:45:36 +0400

    Update naomyst.js

--- naomyst.js
+++ naomyst.js
@@ -6,3 +6,22 @@ ScrollReveal().reveal('.grid-item', {
     origin: 'bottom',
     interval: 100
 });
+// Scroll animation for photography section
+ScrollReveal().reveal('.photo-gallery img', {
+    delay: 300,
+    distance: '20px',
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    interval: 100
+});
+
+// hover for pictures 
+document.querySelectorAll('.photo-gallery img').forEach(img => {
+    img.addEventListener('mouseenter', function() {
+        this.style.transform = 'scale(1.1)';
+    });
+    
+    img.addEventListener('mouseleave', function() {
+        this.style.transform = 'scale(1)';
+    });
+});
